963131e737882c7c48eb8634d68e0be9f27eb3e2
kavya111-op <kavya.25bce10453@vitbhopal.ac.in>
2026-05-18 01:11:51 +0530

    Update taskiq.py
    Co-Authored-By: RITVIKKAMASETTY <ritvikksymp@gmail.com>

--- backend/app/background/taskiq/taskiq.py
+++ backend/app/background/taskiq/taskiq.py
@@ -1,17 +1,25 @@
 import ssl
 
-from taskiq_redis import ListQueueBroker, RedisAsyncResultBackend
+from taskiq_redis import ListQueueBroker, RedisAsyncResultBackend  # type: ignore[import]
 
 from app.config import settings
 
+redis_kwargs = {}
+
 if settings.REDIS_URL.startswith("rediss://"):
-    _ssl_ctx: ssl.SSLContext | None = ssl.create_default_context()
-    assert _ssl_ctx is not None
+    _ssl_ctx = ssl.create_default_context()
+
     _ssl_ctx.check_hostname = False
     _ssl_ctx.verify_mode = ssl.CERT_NONE
-else:
-    _ssl_ctx = None
 
-broker = ListQueueBroker(url=settings.REDIS_URL).with_result_backend(
-    RedisAsyncResultBackend(redis_url=settings.REDIS_URL)
+    redis_kwargs["ssl"] = _ssl_ctx
+
+broker = ListQueueBroker(
+    url=settings.REDIS_URL,
+    redis_kwargs=redis_kwargs,
+).with_result_backend(
+    RedisAsyncResultBackend(
+        redis_url=settings.REDIS_URL,
+        redis_kwargs=redis_kwargs,
+    )
 )
